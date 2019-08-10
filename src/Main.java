b3a728f22d11bc0a213eee5638af641e87eee9c1
Piotr Hołownia <biuro@itcreate.pl>
2019-08-10 13:16:17 +0200

    Create list od Persons

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Piotr",23,"Developer",true));
+        personList.add(new Person("Marek",33,"Developer",true));
+        personList.add(new Person("Michal",23,"Developer",true));
+
 
     }
 }
