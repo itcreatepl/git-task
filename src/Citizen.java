ac138f9b5829838b820734f03e464d2818a110f0
MarcinL1990 <marcinl1990@o2.pl>
2019-08-10 13:30:13 +0200

    Add class Citizen

---
+++ src/Citizen.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Citizen extends Human {
+
+        private String name2;
+        private String address;
+
+    public Citizen(String name, int age, String name2, String address) {
+        super(name, age);
+        this.name2 = name2;
+        this.address = address;
+    }
+}
